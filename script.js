54df572185bc36115e94bf461e1d2afd6c131702
shoooobie <blakehharris@gmail.com>
2021-09-12 02:37:24 -0700

    Added B64 converter

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$(document).ready (() => {
+    $("#b64").keyup( function() {
+        try {
+            $("#pt").text(atob($("#b64").text()));
+        } catch(err){
+            $("#pt").text("There was an error decoding your text!")
+        } 
+    });
+    $("#pt").keyup( function() {
+        try {
+            $("#b64").text(btoa($("#pt").text()));
+        } catch(err){
+            $("#b64").text("There was an error encoding your text!")
+        } 
+    });
+})
+
+
