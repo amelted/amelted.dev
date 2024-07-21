aabcdad5dadf8e436ec7ab9aa2fef59b24323892
unknown <ameliaaharris2@gmail.com>
2024-07-21 03:57:12 -0700

    good one

--- app.ts
+++ app.ts
@@ -4,19 +4,19 @@ import http, {ServerResponse, IncomingMessage} from 'http'
 import fs, {readFileSync} from 'fs'
 const app = express()
 let middleware = (req: Request, res: Response, next: Function) => {
+    let filename = `./static/lol/assets/motivational${req.originalUrl.slice(20,22)}.png`;
     if(req.get('host') == "lol.amelted.dev"){
         if(req.originalUrl.includes("assets")){
-            if(!fs.existsSync(`./static/lol/assets/motivational${req.originalUrl.slice(20,22)}.png`)){
-                res.status(404).send(`you seem lost...`);; 
+            if(!fs.existsSync(filename)){
+                res.statusCode = 404;
+                res.send(`you seem lost...`); 
                 next();return;
             }
                
             res.sendFile(`/static/lol/assets/motivational${req.originalUrl.slice(20,22)}.png`, {root: __dirname});
-            res.end(); 
             next();return;
         } else{
             res.sendFile('/static/lol/index.html', {root: __dirname})
-            res.end();
         }  
     }
     next();
