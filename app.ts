0d1d1ab23bf78b8c4c787a32d8cfd80e3af19909
unknown <ameliaaharris2@gmail.com>
2024-07-20 21:12:17 -0700

    m

--- app.ts
+++ app.ts
@@ -1,6 +1,23 @@
-import express from 'express'
+import express, { Request, Response, request } from 'express'
+import https, {createServer} from 'https'
+import http, {ServerResponse, IncomingMessage} from 'http'
 const app = express()
-const port = 80
 
 app.use(express.static("./static/"));
-app.listen(port, () => console.log(`Web site listening on port ${port}!`))
+
+const httpsServer : any = https.createServer({}, app);
+
+let server = app.listen(443)
+let app80 = express()
+app80.all('*',(request: Request, res: Response) => {
+    res.redirect(`${request.url.replace('http', 'https')}`)
+})
+app80.listen(80)
+// const httpServer : any = createServer({}, (req: IncomingMessage, res: ServerResponse) => {
+//     res.statusCode = 301
+//     res.setHeader('location', "https://amelted.dev")
+// });
+// // httpServer.on('request', (req: Request, res: Response) => )
+// httpServer.listen(80, () => {
+//     console.info(`HTTP Server running on port 80.`);
+// });
