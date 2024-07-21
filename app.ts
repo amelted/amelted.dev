8cb0300c6ba99316e3f2fe439a46e15e52b6fe26
unknown <ameliaaharris2@gmail.com>
2024-07-20 21:33:09 -0700

    haha j x 2

--- app.ts
+++ app.ts
@@ -7,12 +7,14 @@ app.use(express.static("./static/"));
 
 const httpsServer : any = https.createServer({}, app);
 
-let server = app.listen(443)
-// const httpServer : any = createServer({}, (req: IncomingMessage, res: ServerResponse) => {
-//     res.statusCode = 301
-//     res.setHeader('location', "https://amelted.dev")
-// });
-// // httpServer.on('request', (req: Request, res: Response) => )
-// httpServer.listen(80, () => {
-//     console.info(`HTTP Server running on port 80.`);
-// });
+let server = app.listen(443, () =>{
+    console.log("listening!")
+})
+const httpServer : any = createServer({}, (req: IncomingMessage, res: ServerResponse) => {
+    res.statusCode = 301
+    res.setHeader('location', "https://amelted.dev")
+});
+// httpServer.on('request', (req: Request, res: Response) => )
+httpServer.listen(80, () => {
+    console.info(`HTTP Server running on port 80.`);
+});
