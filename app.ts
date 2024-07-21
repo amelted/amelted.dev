39c0e295fb1767251e48c676cd28f1c3b3c3b3dd
unknown <ameliaaharris2@gmail.com>
2024-07-21 12:29:24 -0700

    bigass test no branch lol

--- app.ts
+++ app.ts
@@ -2,37 +2,30 @@ import express, { Request, Response, request, response } from 'express'
 import https, {createServer} from 'https'
 import http, {ServerResponse, IncomingMessage} from 'http'
 import fs, {readFileSync} from 'fs'
+import vhost from 'vhost'
+
 const app = express()
-let middleware = (req: Request, res: Response, next: Function) => {
-    
-    if(req.get('host') != "lol.amelted.dev" || req.originalUrl.includes("assets")){
-        next();return;
-    }
-    res.sendFile('/static/lol/index.html', {root: __dirname});
-    next();return;
-}
-app.all("*", middleware);
+
+let lolApp = express()
+    .use(express.static("./static/lol"));
+
+//app.all("*", middleware);
 app.all("*", (request: Request, response: Response)=>{
     response.status(404).send(`you seem lost...`);
     response.end();
 })
 app.use(express.static("./static/"));
-
+let serv = express()
+    .use(vhost("lol.amelted.dev", lolApp))
+    .use(vhost("amelted.dev", app));
 const httpsServer : any = https.createServer(
     {
         key: readFileSync('server.key'), 
         cert: readFileSync('server.cert')
-    }, app).listen(443, () =>{
-    console.log("443 listening!")
+    }, serv).listen(443, () =>{
+    console.log("443 listening")
 })
-const httpApp = express();
-httpApp.all("*", middleware);
-// httpApp.all("*", (request: Request, response: Response)=>{
-//     response.statusCode = 404
-//     response.send(`you seem lost...`);
-//     response.end();
-// })
-httpApp.use(express.static("./static/"));
 
-const httpServer = http.createServer(httpApp);
-httpServer.listen(80, () => console.log(`80 is on`));
+
+const httpServer = http.createServer(serv)
+    .listen(80, () => console.log(`80 is on`));
