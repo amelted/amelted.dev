e3eb44f1d46033d665eacb4430be3428cc677a40
unknown <ameliaaharris2@gmail.com>
2024-07-21 12:00:02 -0700

    bedtime soon

--- app.ts
+++ app.ts
@@ -5,27 +5,17 @@ import fs, {readFileSync} from 'fs'
 const app = express()
 let middleware = (req: Request, res: Response, next: Function) => {
     
-    if(req.get('host') != "lol.amelted.dev"){
+    if(req.get('host') != "lol.amelted.dev" || req.originalUrl.includes("assets")){
         next();return;
     }
     res.sendFile('/static/lol/index.html', {root: __dirname});
     next();return;
 }
 app.all("*", middleware);
-// app.all("/assets/*", (req, res)=> {
-//     let filename = `./static/lol/assets/motivational${req.originalUrl.slice(20,22)}.png`;
-//     if(!fs.existsSync(filename)){
-//         res.statusCode = 404;
-//         res.send(`you seem lost...`); 
-//         return;
-//     }
-//     res.sendFile(filename, {root: __dirname});
-//     return;
-// })
-// app.all("*", (request: Request, response: Response)=>{
-//     response.status(404).send(`you seem lost...`);
-//     response.end();
-// })
+app.all("*", (request: Request, response: Response)=>{
+    response.status(404).send(`you seem lost...`);
+    response.end();
+})
 app.use(express.static("./static/"));
 
 const httpsServer : any = https.createServer(
