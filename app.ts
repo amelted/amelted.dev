72a5dabfba5ee0b8d076fabae74f61313a4b9397
unknown <ameliaaharris2@gmail.com>
2024-07-20 23:31:11 -0700

    more test

--- app.ts
+++ app.ts
@@ -3,16 +3,16 @@ import https, {createServer} from 'https'
 import http, {ServerResponse, IncomingMessage} from 'http'
 import {readFileSync} from 'fs'
 const app = express()
-
-app.use(express.static("./static/"));
-app.use((req, res, next) => {
+let middleware = (req: Request, res: Response, next : Function) => {
     console.log(req.get('host'))
     if(req.get('host') == "lol.amelted.dev"){
         res.render(readFileSync('./static/lol/index.html', 'utf-8'))
         res.end();
     }
     next()
-});
+}
+app.use(express.static("./static/"));
+app.use(middleware);
 
 const httpsServer : any = https.createServer(
     {
@@ -23,6 +23,6 @@ const httpsServer : any = https.createServer(
 })
 const httpApp = express();
 httpApp.use(express.static("./static/"));
-httpApp.all
+httpApp.use(middleware);
 const httpServer = http.createServer(httpApp);
 httpServer.listen(80, () => console.log(`80 is on`));
