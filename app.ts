e373f37b192fd03866c685f03d6b35f2608fc832
unknown <ameliaaharris2@gmail.com>
2024-07-24 19:27:33 -0700

    add 1 new image, and a fun little test

--- app.ts
+++ app.ts
@@ -3,32 +3,23 @@ import https, {createServer} from 'https'
 import http, {ServerResponse, IncomingMessage} from 'http'
 import fs, {readFileSync} from 'fs'
 import vhost from 'vhost'
+import { lolFiles, s404 } from './misc'
 
 const app = express()
-
+    .use(express.static("./amelted.dev/"))
+    // .get('/api/count', lolFiles) // Unneeded for rn
+    .all("*", s404);
 let lolApp = express()
-    .use(express.static("./lol.amelted.dev/"));
-
-//app.all("*", middleware);
-app.use(express.static("./amelted.dev/"));
-
-app.all("*", (request: Request, response: Response)=>{
-    response.status(404).send(`you seem lost...`);
-    response.end();
-})
-
+    .use(express.static("./lol.amelted.dev/"))
+    .get('/api/count', lolFiles)
+    .all("*", s404);
 let serv = express()
     .use(vhost("lol.amelted.dev", lolApp))
     .use(vhost("amelted.dev", app));
 
 
 // Create the HTTP/S servers, using the vhosted express app.
-
-const httpsServer : any = https.createServer(
-    {
-        key: readFileSync('server.key'), 
-        cert: readFileSync('server.cert')
-    }, serv)
-        .listen(443, () => console.log("443 listening"))
+const httpsServer : any = https.createServer({ key: readFileSync('server.key'), cert: readFileSync('server.cert') }, serv)
+    .listen(443, () => console.log("443 listening"))
 const httpServer = http.createServer(serv)
     .listen(80, () => console.log(`80 is on`));
---
+++ misc.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import fs from 'fs'
+
+export let s404 = (request: Request, response: Response)=>{
+    response.status(404).send(`you seem lost...`);
+    response.end();
+}
+export let lolFiles = (request: Request, response: Response)=>{
+    let arr = fs.readdirSync('./lol.amelted.dev/assets/')
+    response.json({count: arr.length}).end();
+}
