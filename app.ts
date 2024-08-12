d3646aa64fbd8d0b98d3318d922d12355e0f9db0
unknown <ameliaaharris2@gmail.com>
2024-08-12 08:56:21 -0700

    added radio app proxy

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import http, {ServerResponse, IncomingMessage} from 'http'
 import fs, {readFileSync} from 'fs'
 import vhost from 'vhost'
 import { lolFiles, s404 } from './misc'
+import proxy from 'express-http-proxy'
 
 const app = express()
     .use(express.static("./amelted.dev/"))
@@ -13,10 +14,13 @@ let lolApp = express()
     .use(express.static("./lol.amelted.dev/"))
     .get('/api/count', lolFiles)
     .all("*", s404);
+let radioApp = express()
+    .use(proxy("localhost:160/public/melted_jam"))
+    .use('/listen', proxy("localhost:160/listen/melted_jam/radio.mp3"))
 let serv = express()
     .use(vhost("lol.amelted.dev", lolApp))
-    .use(vhost("amelted.dev", app));
-
+    .use(vhost("amelted.dev", app))
+    .use(vhost("radio.amelted.dev"), radioApp);
 
 // Create the HTTP/S servers, using the vhosted express app.
 const httpsServer : any = https.createServer({ key: readFileSync('server.key'), cert: readFileSync('server.cert') }, serv)
